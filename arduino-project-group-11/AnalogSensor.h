479d84e8358d062e94e16cfc25aed1e4f8de014b
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-04-17 21:52:41 +0800

    allow variable number of pins and modes in AnalogSensor class

--- arduino-project-group-11/AnalogSensor.h
+++ arduino-project-group-11/AnalogSensor.h
@@ -1,18 +1,46 @@
 #ifndef ANALOGSENSOR_H
 #define ANALOGSENSOR_H
 
+#include "sensor_structs.h"
+
 // Base class for analog sensors
+template<size_t MaxPins>
 class AnalogSensor {
-private:
-    unsigned int analog_pin;
+protected:
+    unsigned int analog_pins[MaxPins]; // Use a fixed-size array to store pins
+    size_t num_pins; // Track the number of pins
 
 public:
-    // Constructor to initialize analog pin
-    AnalogSensor(const unsigned int pin) : analog_pin(pin) {}
+    // Constructor to initialize digital pins
+    template<typename... Args>
+    AnalogSensor(Args... pins) : num_pins(sizeof...(pins)) {
+        static_assert(sizeof...(pins) <= MaxPins, "Too many pins specified");
+        unsigned int pinsArray[] = {pins...};
+        for (size_t i = 0; i < num_pins; ++i) {
+            analog_pins[i] = pinsArray[i];
+        }
+    }
 
-    // Method to read analog value from the sensor
-    unsigned int read() {
-        return analogRead(analog_pin); 
+    // // Method to initialize pinMode in setup function
+    template<typename... Modes>
+    void init(Modes... modes) {
+        static_assert(sizeof...(Modes) <= MaxPins, "Number of modes exceeds MaxPins");
+
+        int modeArr[] = {modes...};
+
+        for (size_t i = 0; i < sizeof...(Modes); ++i) {
+            pinMode(analog_pins[i], modeArr[i]);
+        }
+    }
+
+    // // Method to read analog value from the sensor
+    unsigned int read(unsigned int pinIndex) {
+        if (pinIndex < num_pins) {
+            return analogRead(analog_pins[pinIndex]);
+        } else {
+            // Handle invalid index
+            return 0;
+        }
     }
 };
 
