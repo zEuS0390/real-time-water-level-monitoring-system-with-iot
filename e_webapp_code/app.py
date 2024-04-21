41da332c2927e83d8a7611c4fe861316f738eec9
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-04-21 21:16:40 +0800

    update frontend and backend

--- e_webapp_code/app.py
+++ e_webapp_code/app.py
@@ -1,35 +1,51 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mqtt import Mqtt
+import paho.mqtt.client as Mqtt
 from flask_socketio import SocketIO
-
 from db import db, User
+import uuid
 
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-mqtt = Mqtt()
+mqtt = Mqtt.Client(Mqtt.CallbackAPIVersion.VERSION2, f"MQTT-SOCKETIO-BRIDGE-{uuid.uuid4()}", clean_session=True)
 socketio = SocketIO()
+
+MQTT_BROKER_URL = 'localhost'
+MQTT_BROKER_PORT = 1883         # default port for non-tls connection
+MQTT_USERNAME = 'user'          # set the username here if you need authentication for the broker
+MQTT_PASSWORD = 'pass123'       # set the password here if the broker demands authentication
+
+def handle_mqtt_connect(client, userdata, flags, reasonCode, properties):
+    if reasonCode == 0: 
+        print('Connected successfully')
+        mqtt.subscribe("group11")
+    else: print('Bad connection. Code:', reasonCode)
+    
+def handle_mqtt_message(client, userdata, message):
+    topic = message.topic
+    payload = message.payload.decode()
+    socketio.emit(topic, payload)
 
 def create_app():
   app = Flask(__name__)
 
-  app.config['TEMPLATES_AUTO_RELOAD'] = True
   app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
   app.config["SECRET_KEY"] = "my-secret"
-  app.config['MQTT_BROKER_URL'] = 'localhost'
-  app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-  app.config['MQTT_USERNAME'] = 'user'  # set the username here if you need authentication for the broker
-  app.config['MQTT_PASSWORD'] = 'pass123'  # set the password here if the broker demands authentication
 
   db.init_app(app)
   bcrypt.init_app(app)
   login_manager.init_app(app)
-  mqtt.init_app(app)
   socketio.init_app(app)
 
   with app.app_context():
     db.create_all()
+
+  mqtt.on_connect = handle_mqtt_connect
+  mqtt.on_message = handle_mqtt_message
+  mqtt.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+  mqtt.connect(MQTT_BROKER_URL, MQTT_BROKER_PORT)
+  mqtt.loop_start()
 
   return app
 
--- e_webapp_code/main.py
+++ e_webapp_code/main.py
@@ -2,7 +2,6 @@ from app import create_app, socketio
 from flask_login import current_user
 from flask_socketio import disconnect
 from views_bp import views_bp
-from app import mqtt
 
 app = create_app()
 
@@ -10,36 +9,11 @@ app = create_app()
 def connect_handler():
     if current_user.is_authenticated:
         print(current_user.username)
-        mqtt.subscribe("group11/rain") # subscribe topic
-        mqtt.subscribe("group11/water") # subscribe topic
-        mqtt.subscribe("group11/distance") # subscribe topic
     else:
         disconnect()
         return False  # not allowed here
-    
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-   if rc == 0:
-        print('Connected successfully')
-   else:
-       print('Bad connection. Code:', rc)
-    
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    topic = message.topic
-    payload = message.payload.decode()
-    print(f"Received message on topic: {topic}, payload: {payload}")
-    if topic == "group11/rain":
-        socketio.emit(topic, payload)
-        print(topic)
-    elif topic == "group11/water":
-        socketio.emit(topic, payload)
-        print(topic)
-    elif topic == "group11/distance":
-        socketio.emit(topic, payload)
-        print(topic)
 
 app.register_blueprint(views_bp)
 
 if __name__=="__main__":
-  socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0', use_reloader=False)
