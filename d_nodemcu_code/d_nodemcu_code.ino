8127eab319a7a7c0ee550cce889ff922c30f2974
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-04-22 13:33:10 +0800

    update nodemcu esp8266 code

--- d_nodemcu_code/d_nodemcu_code.ino
+++ d_nodemcu_code/d_nodemcu_code.ino
@@ -12,29 +12,44 @@ const char* mqtt_password = "pass123";
 WiFiClient espClient;
 PubSubClient client(espClient); 
 
-void connectToWiFiNetwork(const char* ssid, const char* password) {
+void connectToWiFiNetwork(const char* ssid, const char* password, unsigned long timeoutMillis) {
   // Your existing code for connecting to WiFi network
-   delay(10);
+  unsigned long startTime = millis(); // Get the start time
+  
   // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  StaticJsonDocument<400> wifiDoc;
+  wifiDoc["title"] = "wifi connect";
+  wifiDoc["msg"] = ssid;
+  String jsonWifiDoc;
+  serializeJson(wifiDoc, jsonWifiDoc);
+  Serial.println(jsonWifiDoc);
+
+  while (WiFi.status() != WL_CONNECTED && (millis() - startTime) < timeoutMillis) {
     delay(500);
-    Serial.print(".");
   }
 
-  randomSeed(micros());
+  if (WiFi.status() == WL_CONNECTED) {
+    randomSeed(micros());
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    StaticJsonDocument<400> wifiSuccessDoc;
+    wifiSuccessDoc["title"] = "wifi success";
+    wifiSuccessDoc["msg"] = "connected";
+    String wifiSuccessStr;
+    serializeJson(wifiSuccessDoc, wifiSuccessStr);
+    Serial.println(wifiSuccessStr);
+  } else {
+    StaticJsonDocument<400> wifiErrorDoc;
+    wifiErrorDoc["title"] = "wifi error";
+    wifiErrorDoc["msg"] = "timeout reached";
+    String wifiErrorStr;
+    serializeJson(wifiErrorDoc, wifiErrorStr);
+    Serial.println(wifiErrorStr);
+  }
 }
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -45,21 +60,31 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    StaticJsonDocument<400> mqttDoc;
+    mqttDoc["title"] = "mqtt connect";
+    mqttDoc["msg"] = mqtt_server;
+    String jsonMqttDoc;
+    serializeJson(mqttDoc, jsonMqttDoc);
+    Serial.println(jsonMqttDoc);
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("successfully connected to MQTT broker");
-      // Once connected, publish an announcement...
-      // ... and resubscribe
-      // client.subscribe("inTopic");
+      StaticJsonDocument<400> mqttSuccessDoc;
+      mqttSuccessDoc["title"] = "mqtt success";
+      mqttSuccessDoc["msg"] = "connected";
+      String mqttSuccessStr;
+      serializeJson(mqttSuccessDoc, mqttSuccessStr);
+      Serial.println(mqttSuccessStr);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
+      StaticJsonDocument<400> mqttFailedDoc;
+      mqttFailedDoc["title"] = "mqtt failed";
+      mqttFailedDoc["msg"] = "retrying in 5s";
+      String mqttSuccessStr;
+      serializeJson(mqttFailedDoc, mqttSuccessStr);
+      Serial.println(mqttSuccessStr);
       delay(5000);
     }
   }
@@ -67,30 +92,39 @@ void reconnect() {
 
 void setup() {
   Serial.begin(9600);
-  connectToWiFiNetwork(WIFI_NETWORK_DEFAULT_SSID, WIFI_NETWORK_DEFAULT_PASSWORD);
+  connectToWiFiNetwork(WIFI_NETWORK_DEFAULT_SSID, WIFI_NETWORK_DEFAULT_PASSWORD, 10000);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
-  if (!client.connected()) {
+  if (WiFi.status() == WL_CONNECTED && !client.connected()) {
     reconnect();
   }
-  if (Serial.available() > 0) {
+  while (Serial.available() > 0) {
     String jsonStr = Serial.readStringUntil('\n');
-    StaticJsonDocument<200> mainDoc;
+    StaticJsonDocument<800> mainDoc;
     DeserializationError error = deserializeJson(mainDoc, jsonStr);
     if (!error) {
-      if (mainDoc.containsKey("wifi_network_ssid") && mainDoc.containsKey("wifi_network_password")) {
-        WiFi.disconnect();
-        const char* ssid = mainDoc["wifi_network_ssid"];
-        const char* password = mainDoc["wifi_network_password"];
-        connectToWiFiNetwork(ssid, password);
+      if (mainDoc.containsKey("wifi")) {
+
+        if (mainDoc["wifi"].containsKey("ssid") && mainDoc["wifi"].containsKey("password")) {
+          client.disconnect();
+          WiFi.disconnect();
+          const char* ssid = mainDoc["wifi"]["ssid"];
+          const char* password = mainDoc["wifi"]["password"];
+          connectToWiFiNetwork(ssid, password, 10000);
+        }
+        
       }
       client.publish("group11", jsonStr.c_str());
     } else {
-      Serial.print("Parsing failed: ");
-      Serial.println(error.c_str());
+      StaticJsonDocument<400> errorDoc;
+      errorDoc["title"] = "received json error";
+      errorDoc["msg"] = error.c_str();
+      String jsonErrorStr;
+      serializeJson(errorDoc, jsonErrorStr);
+      Serial.println(jsonErrorStr);
     }
   }
   delay(1000);
